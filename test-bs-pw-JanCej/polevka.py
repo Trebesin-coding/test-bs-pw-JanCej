b629e118ab41e39bec699d510f5630df2284e837
JS <84028625+souhrada@users.noreply.github.com>
2026-01-09 19:30:56 +0100

    oprava - polévka
    Fetch ingredients from a webpage using BeautifulSoup and requests.

--- test-bs-pw-JanCej/polevka.py
+++ test-bs-pw-JanCej/polevka.py
@@ -1,13 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
 def main():
+   
+    response = requests.get("https://souhrada.github.io/bsoup-exam/")
 
+    soup = BeautifulSoup(response.content, "html.parser")
+    ingredience = soup.select("h2")
 
+    ingredience = { # chybí rovnítko
+        ingredience[0]. text,
+        ingredience[1]. text,
+        ingredience[2]. text,
+        ingredience[3]. text
+    }
+    if not ingredience:
+        print("nenasel jsem zadne")
+    
+    # proč chybí print ingrediencí?
+       
+   
     # response = requests.get(url)
 
     # BeautifulSoup(response.content, "html.parser") <--- Úkol: popiš krátce, co tohle dělá
+    # zacne zpracovavat stazena data
 
 
 if __name__ == "__main__":
